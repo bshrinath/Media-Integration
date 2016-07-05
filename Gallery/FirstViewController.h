dd7a12c9c7ea5f8490ab56ce1b73c81811213e99
bshrinath <bshrinath@icloud.com>
2016-07-05 12:17:21 -0400

    Initial Commit

---
+++ Gallery/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Gallery
+//
+//  Created by Shrinath on 7/5/16.
+//  Copyright © 2016 cmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Gallery/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Gallery
+//
+//  Created by Shrinath on 7/5/16.
+//  Copyright © 2016 cmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
